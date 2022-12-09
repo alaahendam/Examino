c7e2be7e9a3d2ff79143b2626b776e64df63cb45
alaahendam <alaa.hendam122@gmail.com>
2022-12-10 00:00:34 +0200

    add links commit

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -8,8 +8,20 @@ const Home = () => {
         backgroundColor: "#E7EBF0",
         width: "100%",
         height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
       }}
-    ></div>
+      className="home"
+    >
+      <a href="https://www.behance.net/gallery/126118663/Online-Exam-Case-Study">
+        Ui & Ux Case Study
+      </a>
+      <a href="https://www.behance.net/gallery/126118663/Online-Exam-Case-Study">
+        Github Repo
+      </a>
+    </div>
   );
 };
 export default Home;
