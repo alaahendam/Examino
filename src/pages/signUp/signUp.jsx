3319899c8bdac59a3c3b1cbafb81371204e9f9ec
alaahendam <alaa.hendam122@gmail.com>
2022-12-11 23:41:04 +0200

    sign up

--- src/pages/signUp/signUp.jsx
+++ src/pages/signUp/signUp.jsx
@@ -1,7 +1,79 @@
 import React from "react";
 import "./signUp.css";
-
+import student from "../../images/student.png";
+import teacher from "../../images/teacher.png";
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="signUp">
+      <p className="welcome">Welcome To Examino !</p>
+      <p
+        style={{
+          fontSize: "12px",
+          marginBottom: "15px",
+        }}
+      >
+        sign up to create your account
+      </p>
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        <img src={teacher} alt="" srcset="" />
+        <img src={student} alt="" srcset="" />
+      </div>
+      <input
+        type="text"
+        placeholder="user name"
+        style={{
+          paddingLeft: "10px",
+        }}
+      />
+      <input
+        type="text"
+        placeholder="ID"
+        style={{
+          paddingLeft: "10px",
+        }}
+      />
+      <input
+        type="email"
+        placeholder="Email Address"
+        style={{
+          paddingLeft: "10px",
+        }}
+      />
+      <input
+        type="telephone"
+        placeholder="Telephone"
+        style={{
+          paddingLeft: "10px",
+        }}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        style={{
+          paddingLeft: "10px",
+        }}
+      />
+      <input
+        type="password"
+        placeholder="Confirm Password"
+        style={{
+          paddingLeft: "10px",
+        }}
+      />
+      <input
+        type="button"
+        value="Sign In"
+        style={{
+          background: "linear-gradient(100deg,#A840D1, #56D1D4)",
+          color: "white",
+          cursor: "pointer",
+        }}
+      />
+    </div>
+  );
 };
 export default SignUp;
