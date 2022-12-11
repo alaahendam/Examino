51e493f7ea8fd320b8f58c8ae51befbfcf9f987c
alaahendam <alaa.hendam122@gmail.com>
2022-12-11 22:07:11 +0200

    new style

--- src/pages/signIn/signIn.jsx
+++ src/pages/signIn/signIn.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./signIn.css";
 import loginIcon from "../../images/Login-amico.png";
 
 const SignIn = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="signIn">
       <div className="signInInfo">
@@ -11,18 +13,36 @@ const SignIn = () => {
         <p
           style={{
             fontSize: "12px",
+            marginBottom: "15px",
           }}
         >
           sign in to enter your account
         </p>
-        <input type="text" />
-        <input type="password" />
         <input
-          type="submit"
+          type="text"
+          value={userName}
+          placeholder="user name"
+          style={{
+            paddingLeft: "10px",
+          }}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          placeholder="password"
+          style={{
+            paddingLeft: "10px",
+          }}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="button"
           value="Sign In"
           style={{
             background: "linear-gradient(100deg,#A840D1, #56D1D4)",
             color: "white",
+            cursor: "pointer",
           }}
         />
       </div>
