4f39cd53e021f090fabf1426004410c44f3fd6bb
mo-adel-97 <ma1095394@gmail.com>
2022-12-19 21:48:06 +0200

    Update home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -7,49 +7,51 @@ import { useNavigate } from "react-router-dom";
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <>
-    <div className="home">
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          direction: "rtl",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <img src={home} alt="" srcset="" />
-        <div className="getStartedInfo">
-          <p
-            style={{
-              fontSize: "20px",
-              fontWeight: 600,
-              marginBottom: "10px",
-            }}
-          >
-            Online Exam Builder
-          </p>
-          <p
-            style={{
-              fontSize: "14px",
-              marginBottom: "14px",
-            }}
-          >
-            it is a online exam website that aims to help university students to
-            perform their exams easily and helps professors to set the exam
-            easily
-          </p>
-          <button className="getStartedBtn" onClick={() => navigate("/signIn")}>
-            Get Started
-          </button>
+    <div>
+      <div className="home">
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            direction: "rtl",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <img src={home} alt="" srcset="" />
+          <div className="getStartedInfo">
+            <p
+              style={{
+                fontSize: "20px",
+                fontWeight: 600,
+                marginBottom: "10px",
+              }}
+            >
+              Online Exam Builder
+            </p>
+            <p
+              style={{
+                fontSize: "14px",
+                marginBottom: "14px",
+              }}
+            >
+              it is a online exam website that aims to help university students
+              to perform their exams easily and helps professors to set the exam
+              easily
+            </p>
+            <button
+              className="getStartedBtn"
+              onClick={() => navigate("/signIn")}
+            >
+              Get Started
+            </button>
+          </div>
         </div>
       </div>
-    
+      <div style={{ marginTop: "3.6em" }}>
+        <Sliderr />
+      </div>
     </div>
-     <div style={{marginTop:"3.6em"}}>
-     <Sliderr/>
-    </div>
-    </>
   );
 };
 export default Home;
