2af07b09cdcd91065497c264fc4ecf670eb22ed5
mo-adel-97 <113055631+mo-adel-97@users.noreply.github.com>
2022-12-23 16:19:56 +0200

    FunctionalityForSelctAnswer
    Add function to check correct answer

--- src/component/QuestionDesign/QuestionDesign.jsx
+++ src/component/QuestionDesign/QuestionDesign.jsx
@@ -1,10 +1,20 @@
 import React from "react"
 import { qBank } from "./Questionsbank"
+import { useState } from "react"
 import "./QuestionDesign.css"
 export default function QueistionDesign() {
+ const [score,setScore] = useState(0);
+
+ const [selected,setSelected] = useState("mohamed");
+ 
+
+//  const handleAnswer =(answerr) =>{
+//    {answerr == selected ? setScore(score+1) : null}
+//  }
+
   return (
     <div className="container mt-3">
-      {qBank.map((Q) => ( 
+      {qBank.map((Q) =>  ( 
         <div>
 
          <h4>{Q.questionId} {Q.question}</h4>
@@ -16,14 +26,27 @@ export default function QueistionDesign() {
               {Q.answers.map((answer =>
               <div style={{marginTop:"0.5"}}>
                 <div className="BTNgroup" >
-                  <input
-                  type="checkbox"
-                  name="selectCheckbox"
-                  id="selectCheckbox"
-                />
-                <label style={{paddingLeft:"0.4rem"}} htmlFor="chooseCb" className="form-check-label">
-                  {answer}
-                </label>
+                <label>
+                    <input
+                      type="radio"
+                      value={answer}
+                      name="question"
+                      onChange={(e) => setSelected(e.target.value)}
+                      onClick={()=>{
+                        if(selected == Q.correct){
+                            return(
+                             setScore (score+1)
+                            )
+                        }else{
+                            return(
+                                null
+                            )
+                        }
+                      }}
+                    />
+                    &nbsp;
+                    {answer}
+                  </label>
                 </div><br />
                 </div>
                 ))} 
@@ -36,6 +59,8 @@ export default function QueistionDesign() {
              <button type="submit" className="btn btn-dark">
                Send
              </button>
+             {selected}
+             {/* {score} */}
            </div>
     </div>
   )
