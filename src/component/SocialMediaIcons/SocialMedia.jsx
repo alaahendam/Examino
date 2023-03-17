4813ee2a9067801d80f513b2d19ba698d7679b3e
mo-adel-97 <113055631+mo-adel-97@users.noreply.github.com>
2023-03-17 20:14:47 +0200

    completeContactPage
    complete Contact us Page add real links for social media icons

--- src/component/SocialMediaIcons/SocialMedia.jsx
+++ src/component/SocialMediaIcons/SocialMedia.jsx
@@ -14,25 +14,25 @@ function SocialMedia(){
     return(
         <div className="social-container">
 
-      <a href="https://www.facebook.com/learnbuildteach/"
+      <a href="https://www.facebook.com"
         className="facebook social">
         <FontAwesomeIcon icon={faFacebook} size="2x" />
       </a>
-      <a href="https://www.facebook.com/learnbuildteach/"
+      <a href="https://www.whatsapp.com"
         className="facebook social">
         <FontAwesomeIcon icon={faWhatsapp} size="2x" />
       </a>
-      <a href="https://www.twitter.com/jamesqquick" className="twitter social">
+      <a href="https://www.twitter.com" className="twitter social">
         <FontAwesomeIcon icon={faTwitter} size="2x" />
       </a>
-      <a href="https://www.twitter.com/jamesqquick" className="twitter social">
+      <a href="https://www.snapchat.com/" className="twitter social">
         <FontAwesomeIcon icon={faSnapchat} size="2x" />
       </a>
-      <a href="https://www.instagram.com/learnbuildteach"
+      <a href="https://www.instagram.com"
         className="instagram social">
         <FontAwesomeIcon icon={faInstagram} size="2x" />
       </a>
-      <a href="https://www.instagram.com/learnbuildteach"
+      <a href="https://www.telegram.com"
         className="instagram social">
         <FontAwesomeIcon icon={faTelegram} size="2x" />
       </a>
